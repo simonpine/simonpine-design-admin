e8cecd3cbac59c292d4de84681cfe44d3cd2fe8b
simon pineda <simonpineda0521@gmail.com>
2023-05-11 19:23:04 -0500

    the items and categories charts have been added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Chart from './components/charts';
+import { useState, useEffect } from "react";
+import { collection, getDocs, getFirestore } from "firebase/firestore"
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Chart color={'#82ca9d'} cat1={'category'} />
+        <Chart color={'#82ca9d'} cat1={'items'} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {RouterProvider} from "react-router-dom"
+import { router } from './router';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyA4XL-bMghZWYk4eZS-s-B97Ujxen35QZI",
+  authDomain: "simonpine-design.firebaseapp.com",
+  projectId: "simonpine-design",
+  storageBucket: "simonpine-design.appspot.com",
+  messagingSenderId: "464773155610",
+  appId: "1:464773155610:web:d8208afa3cb36d6f78faa3",
+  measurementId: "G-11LL5VWF31"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
