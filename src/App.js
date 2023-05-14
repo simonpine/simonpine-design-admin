0d7e897beb23e88716adc93a697e64b8c412bf77
simon pineda <simonpineda0521@gmail.com>
2023-05-14 18:55:27 -0500

    the companys chart was added, and the code now is cleaner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import Chart from './components/charts';
 import { useState, useEffect } from "react";
-import { collection, getDocs, getFirestore } from "firebase/firestore"
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 function App() {
   return (
     <div className="App">
-        <Chart color={'#82ca9d'} cat1={'category'} />
-        <Chart color={'#82ca9d'} cat1={'items'} />
+        <Chart color={'#BF9B6F'} cat1={'category'} />
+        <Chart color={'#BF9B6F'} cat1={'items'} />
+        <Chart color={'#BF9B6F'} cat1={'company'} />
     </div>
   );
 }
--- src/components/charts.js
+++ src/components/charts.js
@@ -1,93 +1,65 @@
 import { useState, useEffect } from "react";
 import { collection, getDocs, getFirestore } from "firebase/firestore"
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
-function Chart({color, cat1, cat2}) {
-    const [data1, setData1] = useState('')
-    const [data2, setData2] = useState([])
+function Chart({ color, cat1 }) {
+  const [data2, setData2] = useState([])
 
-    useEffect(() => {
-        const db = getFirestore()
-        const itemsColection2 = collection(db, cat1)
-
-        if(cat1 === 'items') {
-            getDocs(itemsColection2).then((snap) => {
-              const nameOfProducts = (snap.docs.map((c) => {
-                let a = { name: c.data().title, 'Were sold': 0 }
-                return a
-              }))
-              return nameOfProducts
-            }).then((a) => {
-              const itemsColection = collection(db, 'orders')
-              getDocs(itemsColection).then((snap) => {
-                const prov = []
-                snap.docs.map((c) => {
-                  let a = c.data().items
-                  prov.push(a)
-                })
-                prov.flat(1).map(item => {
-                  if(item){
-                    const index = a.findIndex(object => {
-                      return object.name === item.title;
-                    })
-                    a[index]['Were sold'] += item.number
-                  }
-                })
-                setData2(a)
-              })
+  useEffect(() => {
+    const db = getFirestore()
+    const itemsColection2 = collection(db, cat1)
+    let pronoun
+    cat1 === 'items' ? pronoun = 'title' : pronoun = 'name';
+    getDocs(itemsColection2).then((snap) => {
+      const nameOfProducts = (snap.docs.map((c) => {
+        let a = { name: c.data()[pronoun], 'Were sold': 0 }
+        return a
+      }))
+      return nameOfProducts
+    }).then((a) => {
+      const itemsColection = collection(db, 'orders')
+      getDocs(itemsColection).then((snap) => {
+        const prov = []
+        snap.docs.map((c) => {
+          let a = c.data().items
+          prov.push(a)
+        })
+        prov.flat(1).map(item => {
+          if (item) {
+            const index = a.findIndex(object => {
+              return pronoun === "title"? object.name === item[pronoun] : object.name === item[cat1] ;
             })
-        }
-        else{
-            getDocs(itemsColection2).then((snap) => {
-                const nameOfProducts = (snap.docs.map((c) => {
-                  let a = { name: c.data().name, 'Were sold': 0 }
-                  return a
-                }))
-                return nameOfProducts
-              }).then((a) => {
-                const itemsColection = collection(db, 'orders')
-                getDocs(itemsColection).then((snap) => {
-                  const prov = []
-                  snap.docs.map((c) => {
-                    let a = c.data().items
-                    prov.push(a)
-                  })
-                  prov.flat(1).map(item => {
-                    if(item){
-                      const index = a.findIndex(object => {
-                        return object.name === item.category;
-                      })
-                      a[index]['Were sold'] += item.number
-                    }
-                  })
-                  setData2(a)
-                })
-              })
-        }
-    }, [])
-    return (
-      <>
-          <BarChart
-            width={500}
-            height={300}
-            data={data2}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" tick=""/>
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            {/* <Bar dataKey="pv" fill="#8884d8" /> */}
-            <Bar dataKey="Were sold" fill={color} />
-          </BarChart>
-      </>
-    );
-  }
-  
-  export default Chart;
+            a[index]['Were sold'] += item.number
+          }
+        })
+        a.map((item) => {
+          return cat1 === 'items' ? item.name = `${item.name}` : item.name = `${item.name} products`
+        })
+        setData2(a)
+      })
+    })
+  }, [])
+  return (
+    <>
+      <BarChart
+        width={500}
+        height={300}
+        data={data2}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 1" />
+        <XAxis dataKey="name" tick="" />
+        <YAxis />
+        <Tooltip itemStyle={{ backgroundColor: '#0c0c0c' }} wrapperStyle={{ backgroundColor: '#0c0c0c' }} contentStyle={{ backgroundColor: '#0c0c0c' }} />
+        <Bar dataKey="Were sold" fill={color} />
+      </BarChart>
+    </>
+  );
+}
+
+export default Chart;
